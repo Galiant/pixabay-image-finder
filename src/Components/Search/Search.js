4711112f54a3c86e87c223e75ba39c856a6062d1
Galiant <antonijodev@gmail.com>
2019-10-21 22:11:38 +0100

    Refactoring search component

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,41 +1,41 @@
 import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
+import TextField from '@material-ui/core/TextField';
 
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    padding: '2px 4px',
+  textField: {
     display: 'flex',
     alignItems: 'center',
     alignSelf: 'stretch',
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
-  },
-  iconButton: {
-    padding: 10,
-  },
+  }
 }));
 
 const Search = () => {
+
   const classes = useStyles();
 
+  const [values, setValues] = React.useState({
+    searchText: '',
+    amount: 15,
+    apiUrl: 'https://pixabay.com/api/',
+    apiKey: '540549-d25cb47a0eadb7d4acfea6805',
+    images: []
+  });
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
   return (
-    <Paper className={classes.root}>
-      <InputBase
-        className={classes.input}
-        placeholder="Search For Images"
-        inputProps={{ 'aria-label': 'search for images' }}
-      />
-      <IconButton className={classes.iconButton} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-    </Paper>
+     <TextField
+      name="searchText"
+      value={values.searchText}
+      onChange={handleChange('searchText')}
+      label="Search for images"
+      fullWidth={true}
+      className={classes.textField}
+    />
   );
 }
 
